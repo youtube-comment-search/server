6d40074978e245c3f1804ba38a75b6f05691a61c
ssongjay <jay5326@naver.com>
2023-07-11 15:25:22 +0900

    🔧스프링 프로젝트 initialize

---
+++ src/main/java/hello/youtubecommentsearch/YoutubeCommentSearchApplication.java
@@ -0,0 +1,13 @@
+package hello.youtubecommentsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeCommentSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeCommentSearchApplication.class, args);
+	}
+
+}
